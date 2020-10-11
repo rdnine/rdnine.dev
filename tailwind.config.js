8a3d598b621429df105f95b8179f2a26ba898906
rdnine <rafael@rdnine.dev>
2020-10-11 02:42:02 +0100

    Tailwindcss config with custom classes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        backgroundColor: () => ({
+            dark: '#242432',
+        }),
+        minHeight: {
+            '100vh': '100vh',
+        },
+        padding: {
+            'lr-10vw': '0 10vw',
+        },
+    },
+    fontFamily: {
+        display: ['Roboto Mono', 'sans-serif'],
+        body: ['Roboto Mono', 'sans-serif'],
+    },
+}
