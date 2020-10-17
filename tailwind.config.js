ad7df0519562cdbd5006f036217157118fc0e4ae
rdnine <rafael@rdnine.dev>
2020-10-17 23:31:13 +0100

    Fix background custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    theme: {
-        backgroundColor: () => ({
-            dark: '#242432',
-        }),
-        minHeight: {
-            '100vh': '100vh',
-        },
-        padding: {
-            'lr-10vw': '0 10vw',
-        },
+  theme: {
+    backgroundColor: () => ({
+      dark: 'rgba(36,36,35,1)',
+    }),
+    minHeight: {
+      '100vh': '100vh',
     },
-    fontFamily: {
-        display: ['Roboto Mono', 'sans-serif'],
-        body: ['Roboto Mono', 'sans-serif'],
+    padding: {
+      'lr-10vw': '0 10vw',
     },
+  },
+  fontFamily: {
+    display: ['Roboto Mono', 'sans-serif'],
+    body: ['Roboto Mono', 'sans-serif'],
+  },
 }
