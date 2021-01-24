7f1aab3f5fa75811343fd1674795d0016f5b01ab
Rafael Duarte <rafael@rdnine.dev>
2021-01-24 22:14:21 +0000

    update: tailwind config
    Added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
     theme: {
         backgroundColor: () => ({
             dark: 'rgba(36,36,35,1)',
+            lighter: 'rgba(207, 219, 213, 1)',
+            grey: 'rgba(51, 53, 51, 1)',
         }),
         minHeight: {
             '100vh': '100vh',
