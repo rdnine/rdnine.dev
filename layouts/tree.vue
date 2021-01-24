8f32c8c164b9901e65a322bb4589ce870533c4a1
Rafael Duarte <rafael@rdnine.dev>
2021-01-24 22:13:47 +0000

    add: tree layout

---
+++ layouts/tree.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-100 min-h-100vh bg-grey">
+        <Nuxt />
+    </div>
+</template>
+
+<style lang="scss">
+html {
+    font-family: 'Roboto Mono', sans-serif;
+    font-size: 10px; /* BASE FONT-SIZE FOR EASIER rem MESURE */
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+    margin: 0;
+}
+</style>
