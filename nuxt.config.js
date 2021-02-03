fad53eab6705213a7cdc42a4e63bb831c79ac8a9
Rafael Duarte <rafael@rdnine.dev>
2021-02-03 23:51:47 +0000

    chore: lint fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,103 +1,103 @@
 export default {
-    // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-    ssr: false,
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
 
-    // Target (https://go.nuxtjs.dev/config-target)
-    target: 'static',
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
 
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-        htmlAttrs: {
-            lang: 'en',
-        },
-        title: 'Rafael Duarte | Web Developer & Tech Lover',
-        meta: [
-            { charset: 'utf-8' },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-            },
-            {
-                name: 'keywords',
-                content:
-                    'developer, coder, programmer, web, web developer, tech, vue, nuxt, php, full-stack, front-end, back-end',
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content:
-                    "I'm a Web Developer from Portugal, learning and building as much as I can. Come and see the goddies!",
-            },
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            {
-                rel: 'stylesheet',
-                href:
-                    'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;600;700&display=swap',
-            },
-        ],
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    htmlAttrs: {
+      lang: 'en',
     },
-
-    // Global CSS (https://go.nuxtjs.dev/config-css)
-    css: ['~/assets/sass/app.scss'],
-
-    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [],
-
-    // Auto import components (https://go.nuxtjs.dev/config-components)
-    components: {
-        dirs: [
-            '~/components',
-            {
-                path: '~/components/links',
-                prefix: 'Links',
-            },
-        ],
-    },
-
-    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: [
-        // https://go.nuxtjs.dev/typescript
-        '@nuxt/typescript-build',
-        // https://go.nuxtjs.dev/tailwindcss
-        '@nuxtjs/tailwindcss',
+    title: 'Rafael Duarte | Web Developer & Tech Lover',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        name: 'keywords',
+        content:
+          'developer, coder, programmer, web, web developer, tech, vue, nuxt, php, full-stack, front-end, back-end',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          "I'm a Web Developer from Portugal, learning and building as much as I can. Come and see the goddies!",
+      },
     ],
-
-    // Modules (https://go.nuxtjs.dev/config-modules)
-    modules: [
-        // https://go.nuxtjs.dev/content
-        '@nuxt/content',
-        '@nuxtjs/axios',
-        '@nuxtjs/proxy',
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;600;700&display=swap',
+      },
     ],
+  },
 
-    axios: {
-        baseURL: 'https://api.rdnine.dev',
-    },
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: ['~/assets/sass/app.scss'],
 
-    // Content module configuration (https://go.nuxtjs.dev/config-content)
-    content: {
-        liveEdit: false,
-    },
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
 
-    // Build Configuration (https://go.nuxtjs.dev/config-build)
-    build: {},
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: {
+    dirs: [
+      '~/components',
+      {
+        path: '~/components/links',
+        prefix: 'Links',
+      },
+    ],
+  },
 
-    render: {
-        // Setting up cache for 'static' directory - a year in milliseconds
-        // https://web.dev/uses-long-cache-ttl
-        static: {
-            maxAge: 60 * 60 * 24 * 365 * 1000,
-        },
-    },
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
 
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+  ],
+
+  axios: {
+    baseURL: 'https://api.rdnine.dev',
+  },
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {
+    liveEdit: false,
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+
+  render: {
+    // Setting up cache for 'static' directory - a year in milliseconds
+    // https://web.dev/uses-long-cache-ttl
     static: {
-        prefix: false,
+      maxAge: 60 * 60 * 24 * 365 * 1000,
     },
+  },
 
-    layoutTransition: {
-        name: 'layout-fade',
-        mode: 'out-in',
-    },
+  static: {
+    prefix: false,
+  },
+
+  layoutTransition: {
+    name: 'layout-fade',
+    mode: 'out-in',
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    theme: {
-        backgroundColor: () => ({
-            dark: 'rgba(36,36,35,1)',
-            lighter: 'rgba(207, 219, 213, 1)',
-            grey: 'rgba(51, 53, 51, 1)',
-        }),
-        minHeight: {
-            '100vh': '100vh',
-        },
-        padding: {
-            'lr-10vw': '0 10vw',
-        },
+  theme: {
+    backgroundColor: () => ({
+      dark: 'rgba(36,36,35,1)',
+      lighter: 'rgba(207, 219, 213, 1)',
+      grey: 'rgba(51, 53, 51, 1)',
+    }),
+    minHeight: {
+      '100vh': '100vh',
     },
-    fontFamily: {
-        display: ['Roboto Mono', 'sans-serif'],
-        body: ['Roboto Mono', 'sans-serif'],
+    padding: {
+      'lr-10vw': '0 10vw',
     },
+  },
+  fontFamily: {
+    display: ['Roboto Mono', 'sans-serif'],
+    body: ['Roboto Mono', 'sans-serif'],
+  },
 }
