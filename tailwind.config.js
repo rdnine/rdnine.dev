c4f103711449b72fa48d11e4eb32f403b7ac4237
Rafael Duarte <rafael@rdnine.dev>
2021-02-06 23:39:07 +0000

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ module.exports = {
       lighter: 'rgba(207, 219, 213, 1)',
       grey: 'rgba(51, 53, 51, 1)',
     }),
-    minHeight: {
-      '100vh': '100vh',
-    },
-    padding: {
-      'lr-10vw': '0 10vw',
-    },
   },
   fontFamily: {
     display: ['Roboto Mono', 'sans-serif'],
