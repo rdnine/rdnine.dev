aa4bbbd6834a1943cf94062aed2b752d872121ab
Rafael Duarte <rafael@rdnine.dev>
2021-02-06 23:40:19 +0000

    feat: cv update
    Update layout + aside init

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,11 +1,15 @@
 <template>
-  <main></main>
+  <main id="cv" class="w-full h-screen max-w-screen-xl mx-auto">
+    <aside class="absolute h-full w-32">
+
+    </aside>
+  </main>
 </template>
 
 <script>
 import Vue from 'vue'
 
 export default Vue.extend({
-  layout: 'fluid',
+  layout: 'fluid-grey',
 })
 </script>
