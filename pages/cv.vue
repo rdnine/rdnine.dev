5d015db0a56a2a878ad2fdb69db81f6b6a510d7b
Rafael Duarte <rafael@rdnine.dev>
2021-02-06 23:58:59 +0000

    feat: update cv page

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,7 +1,19 @@
 <template>
   <main id="cv" class="w-full h-screen max-w-screen-xl mx-auto">
-    <aside class="absolute h-full w-32">
+    <aside class="absolute top-0 bottom-0 h-90c my-auto w-2/5 flex">
+      <div class="nav bg-dark w-48 h-90c my-auto">
 
+      </div>
+      <div class="profile h-full flex content-center">
+        <div class="profile__picture">
+
+        </div>
+        <div class="profile__info">
+          <figure>
+            <img src="side.jpg" alt="Me" />
+          </figure>
+        </div>
+      </div>
     </aside>
   </main>
 </template>
