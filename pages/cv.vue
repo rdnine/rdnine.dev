07d38e1dd736e727b0b12e7af15661782f419354
Rafael Duarte <rafael@rdnine.dev>
2021-02-07 00:28:05 +0000

    feat: update aside in cv page

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,18 +1,37 @@
 <template>
   <main id="cv" class="w-full h-screen max-w-screen-xl mx-auto">
     <aside class="absolute top-0 bottom-0 h-90c my-auto w-2/5 flex">
-      <div class="nav bg-dark w-48 h-90c my-auto">
-
-      </div>
-      <div class="profile h-full flex content-center">
-        <div class="profile__picture">
-
+      <div class="nav bg-dark w-32 h-90c my-auto relative">
+        <div
+          class="absolute top-0 w-full h-32 flex content-center justify-center"
+        >
+          <button class="block focus:outline-none hover:outline-none">
+            <img src="profile.svg" alt="Profile" width="30" height="30" />
+          </button>
         </div>
-        <div class="profile__info">
-          <figure>
-            <img src="side.jpg" alt="Me" />
+        <div
+          class="absolute bottom-0 w-full h-32 flex content-center justify-center"
+        >
+          <a
+            href="cv-2021.pdf"
+            download
+            class="block focus:outline-none hover:outline-none flex content-center justify-center"
+          >
+            <img src="download.svg" alt="Profile" width="30" height="30" />
+          </a>
+        </div>
+      </div>
+      <div class="profile h-full flex-1">
+        <div class="profile__picture h-full w-full flex relative">
+          <figure class="h-full w-full">
+            <img
+              src="side_dark.jpg"
+              alt="Me"
+              class="h-full w-full object-center object-cover"
+            />
           </figure>
         </div>
+        <div class="profile__info"></div>
       </div>
     </aside>
   </main>
