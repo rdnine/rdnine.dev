2c98e62f644e2a11200b25e38bfc01717af4840c
Rafael Duarte <rafaeljfduarte@gmail.com>
2021-02-20 00:30:57 +0000

    chore: update tailwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   theme: {
     backgroundColor: () => ({
       dark: 'rgba(36,36,35,1)',
@@ -11,7 +18,10 @@ module.exports = {
       },
       colors: {
         'sublime-yellow': '#FEDA6A',
-      }
+      },
+      bottom: {
+        '4/5': '80%',
+      },
     },
   },
   fontFamily: {
